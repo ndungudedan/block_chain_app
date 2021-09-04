a33b764bf2cba4b85a52a2ed685b2f9d951c685b
ndungudedan <dnkibere@gmail.com>
2021-09-04 15:39:10 +0300

    init

---
+++ android/app/src/main/kotlin/com/example/block_chain/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.block_chain
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/common/ui_helpers.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+// horizontal spacing
+Widget horizontalSpaceTiny = SizedBox(width: 5.0 );
+Widget horizontalSpaceSmall = SizedBox(width: 10.0 );
+Widget horizontalSpaceRegular = SizedBox(width: 18.0 );
+Widget horizontalSpaceMedium = SizedBox(width: 25.0 );
+Widget horizontalSpaceLarge = SizedBox(width: 50.0 );
+
+// vertical spacing
+Widget verticalSpaceTiny = SizedBox(height: 5.0  );
+Widget verticalSpaceSmall = SizedBox(height: 10.0  );
+Widget verticalSpaceRegular = SizedBox(height: 18.0  );
+Widget verticalSpaceMedium = SizedBox(height: 25.0  );
+Widget verticalSpaceLarge = SizedBox(height: 50.0  );
